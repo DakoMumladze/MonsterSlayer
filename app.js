7871ed37020d0842d6bdaffb876a184bac901107
Dako Mumladze <dmumladze@corp.europe-bet.com>
2019-09-02 14:04:09 +0400

    added log function to all buttons

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function () {
             let damage = this.calculateGame(3, 10);
@@ -25,7 +26,12 @@ new Vue({
             this.monsterAttacks();
         },
         specialAttack: function () {
-            this.monsterHealth -= this.calculateGame(10, 20);
+            let damage = this.calculateGame(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
             if (this.checkWin()){
                 return;
             }
@@ -37,10 +43,15 @@ new Vue({
             }else{
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for 10'
+            }); 
             this.monsterAttacks();
         },
         giveUp: function () {
             this.gameIsRunning = false;
+            this.turns = [];
         },
         monsterAttacks: function () {
             let damage = this.calculateGame(5, 12);
