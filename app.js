fe00077f9bf5156b3300310fe2201bfabdea8bf9
Dako Mumladze <dmumladze@corp.europe-bet.com>
2019-09-02 13:17:30 +0400

    implemented attack button functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-new Vue ({
+new Vue({
     el: '#app',
     data: {
         playerHealth: 100,
@@ -6,10 +6,49 @@ new Vue ({
         gameIsRunning: false
     },
     methods: {
-        startGame: function(){
+        startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function () {
+            this.monsterHealth -= this.calculateGame(3, 10);
+
+            this.playerHealth -= this.calculateGame(5, 12);
+            this.checkWin();
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
+        },
+        calculateGame: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function () {
+
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won ! New Game?')) {
+                    this.startGame();
+                }
+                else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }else if (this.playerHealth <= 0) {
+                if (confirm('You lost ! New Game?')) {
+                    this.startGame();
+                }
+                else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
-})
+});
